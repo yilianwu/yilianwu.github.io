a10ba21d0101bb0114c3c6521ff43b7ed667d406
yilianwu <itsgoogl3r@gmail.com>
2019-12-06 23:58:06 +0800

    rebuilding site Fri Dec  6 23:58:06 CST 2019

--- js/custom_script_1.js
+++ js/custom_script_1.js
@@ -1,9 +1,9 @@
 $(function(){
-    var sideslider = $(‘[data-toggle=collapse-side]’);
-    var get_sidebar = sideslider.attr(‘data-target-sidebar’);
-    var get_content = sideslider.attr(‘data-target-content’);
+    var sideslider = $('[data-toggle=collapse-side]');
+    var get_sidebar = sideslider.attr('data-target-sidebar');
+    var get_content = sideslider.attr('data-target-content');
     sideslider.click(function(event){
-    $(get_sidebar).toggleClass(‘in’);
-    $(get_content).toggleClass(‘out’);
+    $(get_sidebar).toggleClass('in');
+    $(get_content).toggleClass('out');
     });
     });
