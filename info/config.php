695e7fc63d4ece8666273f3f113295c2ed1cd3a2
yilianwu <itsgoogl3r@gmail.com>
2020-08-28 16:38:54 +0800

    rebuilding site Fri Aug 28 16:38:54 CST 2020

--- info/config.php
+++ info/config.php
@@ -2,7 +2,7 @@
 // Paste your long lived access token here
 $token = "IGQVJXTHNGNXVKMXoydGIwYnRBMFAxVTlyNWpKNnFQSEtrdDJ1d01Bek5Cb2R6TGpSWExQUUoyV3d1QVlmcjdHYjhGWU16c0s4dGhSVnRxOWY3SldJOHB3al9LZAVlxUTZAUSk1OR2J2RDNNS3ZAlYUFIcgZDZD";
 // Paste your site url. Example: http://yoursite.com
-$site = "https://yilianwu.io";
+$site = "https://yilianwu.io/info";
 // Paste your fontawesome kit url. Example: https://kit.fontawesome.com/{kit-name}.js
 $fontawesome = "https://kit.fontawesome.com/6a1a745332.js";
 ?>
