021323871db1cc35e39f9851886678a18b327080
yilianwu <itsgoogl3r@gmail.com>
2020-08-28 16:56:27 +0800

    rebuilding site Fri Aug 28 16:56:27 CST 2020

--- info/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Paste your long lived access token here
-$token = "IGQVJVSkxxNm50Qk40MEp1eWxtSndMaEZAEVWF1VGpLS1VsOHZAMQUo0R0prckoyUllhT0k2bjI3dVB0c3d6UElNdVN6aGMxWkRXeWp4NHgybmphNlFZATldkSmM4UEh6UV8zMDFneFFKWEx1eTAzLTNIYQZDZD";
-// Paste your site url. Example: http://yoursite.com
-$site = "https://yilianwu.io/info";
-// Paste your fontawesome kit url. Example: https://kit.fontawesome.com/{kit-name}.js
-$fontawesome = "https://kit.fontawesome.com/6a1a745332.js";
-?>
