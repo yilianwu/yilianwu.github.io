85193144fa99e5acaa686f88883183ecc8ba7074
yilianwu <itsgoogl3r@gmail.com>
2020-09-01 14:53:09 +0800

    rebuilding site Tue Sep  1 14:53:09 CST 2020

--- js/custom_script_1.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    var sideslider = $('[data-toggle=collapse-side]');
-    var get_sidebar = sideslider.attr('data-target-sidebar');
-    var get_content = sideslider.attr('data-target-content');
-    sideslider.click(function(event){
-    $(get_sidebar).toggleClass('in');
-    $(get_content).toggleClass('out');
-    });
-    });
